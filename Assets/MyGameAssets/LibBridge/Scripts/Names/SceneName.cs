a6125034b8c4cf976d47caac2f79c6162aec5066
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-07-18 18:33:14 +0900

    VMUnityLibを追加してビルドできるようにしました。

---
+++ Assets/MyGameAssets/LibBridge/Scripts/Names/SceneName.cs
@@ -0,0 +1,13 @@
+﻿/// <summary>
+/// シーン名を定数で管理するクラス
+/// </summary>
+public static class SceneName
+{
+	public const string root = "root";
+	public const string debugSceneRoot = "debugSceneRoot";
+	public const string demo = "demo";
+	public const string title = "title";
+	public const string stage1 = "stage1";
+	public const string stage2 = "stage2";
+	public const string stage3 = "stage3";
+}
