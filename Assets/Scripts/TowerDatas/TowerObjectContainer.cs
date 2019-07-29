a407b440fe5eae2fc74ac3d8bf8eb5c9c42e11d8
tricore-03 <tricore.as03@gmail.com>
2019-07-29 15:19:48 +0900

    ウサギの抽選処理を作成
    ウサギの出現抽選処理を作成した。
    抽選処理のテストのため、モチとウサギとレアリティの仮のアセットファイルを作成しているため追加ファイルが多少多くなっています。
    
    【確認ファイル】
    TowerObjectContainer.cs
    （タワー関連のオブジェクトを持ったスクリプト。最初のオブジェクトのロードもここで行う）
    
    RabbitRotteryMachine.cs
    （ウサギの出現抽選処理を行うスクリプト）

---
+++ Assets/Scripts/TowerDatas/TowerObjectContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VMUnityLib;
+
+/// <summary>
+/// タワー関連のオブジェクトを保存しておくコンテナクラス
+/// </summary>
+public class TowerObjectContainer : SingletonMonoBehaviour<TowerObjectContainer>
+{
+    [SerializeField]
+    string mochiDataPath = default;
+
+    [SerializeField]
+    string rabbitDataPath = default;
+
+    [SerializeField]
+    string rabbitRarityDataPath = default;
+
+    public IdentifiedDataManager<MochiData>        MochiDataManager        { get; private set; }
+    public IdentifiedDataManager<RabbitData>       RabbitDataManager       { get; private set; }
+    public IdentifiedDataManager<RabbitRarityData> RabbitRarityDataManager { get; private set; }
+
+    /// <summary>
+    /// 全てのデータを読み込む
+    /// </summary>
+    void Awake()
+    {
+        MochiDataManager = new IdentifiedDataManager<MochiData>(mochiDataPath);
+        MochiDataManager.LoadData();
+
+        RabbitDataManager = new IdentifiedDataManager<RabbitData>(rabbitDataPath);
+        RabbitDataManager.LoadData();
+
+        RabbitRarityDataManager = new IdentifiedDataManager<RabbitRarityData>(rabbitRarityDataPath);
+        RabbitRarityDataManager.LoadData();
+    }
+}
