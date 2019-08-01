83218de9b4253424b0f15a1e97d5372a37d518a5
tricore-05 <tricore.as05@gamil.com>
2019-08-01 15:18:54 +0900

    コード修正

--- Assets/MyGameAssets/Script/Ad/AdMob/AdManager.cs
+++ Assets/MyGameAssets/Script/Ad/AdMob/AdManager.cs
@@ -190,17 +190,5 @@ public class AdManager : SingletonMonoBehaviour<AdManager>
         {
             nendInterstitial.Show();
         }
-
-        // 表示回数をカウント
-        showCount++;
-        // 5回毎(動画リワードの番が来るたび)に初期化
-        if (showCount > RewardCount)
-        {
-            showCount = 1;
-        }
-
-        // 表示回数をセーブ
-        PlayerPrefs.SetInt(ShowCountKey, showCount);
-        PlayerPrefs.Save();
     }
 }
