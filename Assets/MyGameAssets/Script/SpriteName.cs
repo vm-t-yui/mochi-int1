5399da4dc5fe02f16b82f08c78d5332c4c15b2bc
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-08-02 17:55:29 +0900

    図鑑の表示切替クラス作成、タイトル用のスプライトセッタークラス作成

---
+++ Assets/MyGameAssets/Script/SpriteName.cs
@@ -0,0 +1,18 @@
+﻿
+/// <summary>
+/// スプライト名の定数クラス
+/// </summary>
+public class SpriteName
+{
+    public const string TitleLogo = "";
+    public const string PictureBookButton = "pictureBook";
+    public const string SettingButton = "setting";
+    public const string LeaderboardButton = "leaderboard";
+    public const string ShareButton = "share";
+    public const string Rarity1 = "rarity1";
+    public const string Rarity2 = "rarity2";
+    public const string Rarity3 = "rarity3";
+    public const string Rarity4 = "rarity4";
+    public const string Rarity5 = "rarity5";
+    public const string Rarity0 = "rarity0";
+}
