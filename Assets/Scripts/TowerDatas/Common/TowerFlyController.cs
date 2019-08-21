ad43693249c28e67e06b6cd0c53e8f9b3e6f90c4
tricore-03 <tricore.as03@gmail.com>
2019-08-21 12:08:56 +0900

    不足コメント追加
    追加したコメントが保存できておらず、反映されていなかったので修正

--- Assets/Scripts/TowerDatas/Common/TowerFlyController.cs
+++ Assets/Scripts/TowerDatas/Common/TowerFlyController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
 /// タワーが飛ばされる処理を制御
+/// TODO : タワー全体の不具合を修正した影響で、こちらの処理でエラーが出始めたので一度処理を全て削除しました。
+///        次回、修正していきたいと思います。
 /// </summary>
 public class TowerFlyController : MonoBehaviour
 {
