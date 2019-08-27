f021bb2e58835a3d1989c695da692a72179cc4ef
tricore-05 <tricore.as05@gamil.com>
2019-08-27 13:13:19 +0900

    ギャラリーの表示非表示修正、うさぎコンプリート時動画広告勧誘停止

--- Assets/MyGameAssets/LibBridge/Scripts/Data/UserData/PlayData.cs
+++ Assets/MyGameAssets/LibBridge/Scripts/Data/UserData/PlayData.cs
@@ -5,6 +5,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class PlayData
 {
@@ -32,4 +33,19 @@ public class PlayData
     public int PunchCount          { get { return punchCount; }       set { punchCount = value; } }
     public bool IsReward           { get { return isReward; }         set { isReward = value; } }
     public bool[] IsReleasedRabbit { get { return isReleasedRabbit; } set { isReleasedRabbit = value; } }
+
+    /// <summary>
+    /// うさぎをコンプリートしているかどうか
+    /// </summary>
+    public bool RabbitComplete()
+    {
+        // NOTE:Distinct().Count() == 1は重複が1つなら(配列の全ての要素が同じ値なら)trueを返すという条件式で
+        //      うさぎをコンプリートしていれば配列のすべてがtrueなり、重複が1となるのでこの条件式が通る
+        if (IsReleasedRabbit.Distinct().Count() == 1)
+        {
+            return true;
+        }
+
+        return false;
+    }
 }
---
+++ Assets/MyGameAssets/Script/GalleryActivator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ギャラリーの表示kルアス
+/// </summary>
+public class GalleryActivator : MonoBehaviour
+{
+    [SerializeField]
+    Timer timer = default;              // タイマークラス
+
+    [SerializeField]
+    GameObject[] gallerys = default;    // ギャラリー達
+
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    void Update()
+    {
+        // タイムアップ時に非表示にする
+        if (timer.IsTimeup)
+        {
+            SetActive(false);
+        }
+        // それ以外は表示
+        else
+        {
+            SetActive(true);
+        }
+    }
+
+    /// <summary>
+    /// 表示関数
+    /// </summary>
+    /// <param name="flg">trueなら表示、falseなら非表示</param>
+    void SetActive(bool flg)
+    {
+        for (int i = 0; i < gallerys.Length; i++)
+        {
+            if (flg)
+            {
+                gallerys[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                gallerys[i].gameObject.SetActive(false);
+            }
+        }
+    }
+}
