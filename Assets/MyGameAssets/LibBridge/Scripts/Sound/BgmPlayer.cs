1b28ff3738d8ad0e63c9dfa008ef4f2ad709a953
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-08-29 16:02:46 +0900

    BgmPlayer.csにフィーバータイム用の処理を追加

--- Assets/MyGameAssets/LibBridge/Scripts/Sound/BgmPlayer.cs
+++ Assets/MyGameAssets/LibBridge/Scripts/Sound/BgmPlayer.cs
@@ -2,6 +2,7 @@
 /*!    \brief  指定BGMを再生する.
 *******************************************************************************/
 
+using System.Collections;
 using UnityEngine;
 using PathologicalGames;
 
@@ -13,9 +14,13 @@ namespace VMUnityLib
         SpawnPool bgmPool;                             // BGMのプール
 
         AudioSource spawnedBgm = new AudioSource();    // 再生中のBGM
+        AudioSource pauseBGM   = new AudioSource();    // 一時停止中のBGM
 
         bool isResult       = false;                   // リザルトフラグ
         bool isPlayedResult = false;                   // リザルトBGM再生完了フラグ
+
+        [SerializeField]
+        float FadeTime = 2f;                           // フェードイン・フェードアウトにかける時間
 
         /// <summary>
         /// 再生.
@@ -35,6 +40,16 @@ namespace VMUnityLib
             // AudioSourceを取得
             spawnedBgm = spawnedSeTrans.GetComponent<AudioSource>();
 
+            // ミュート時はボリュームを０に、そうでなければ設定データのBGMボリュームを設定
+            if (GameDataManager.Inst.SettingData.IsBgmMute)
+            {
+                spawnedBgm.volume = 0;
+            }
+            else
+            {
+                spawnedBgm.volume = GameDataManager.Inst.SettingData.BgmVolume;
+            }
+
             // 再生
             spawnedBgm.Play();
         }
@@ -50,21 +65,153 @@ namespace VMUnityLib
         }
 
         /// <summary>
+        /// 一時停止
+        /// </summary>
+        public void PauseBgm()
+        {
+            if (!spawnedBgm.isPlaying) { return; }
+
+            spawnedBgm.Pause();
+
+            pauseBGM = spawnedBgm;
+        }
+
+        /// <summary>
+        /// 一時停止していたBGMを再生
+        /// </summary>
+        public void ReStartBgm()
+        {
+            pauseBGM.Play();
+
+            spawnedBgm = pauseBGM;
+        }
+
+        /// <summary>
+        /// フェードイン
+        /// </summary>
+        void FadeIn()
+        {
+            // ミュート時は処理を抜ける
+            if (GameDataManager.Inst.SettingData.IsBgmMute) { return; }
+
+            StartCoroutine(_FadeIn());
+        }
+        IEnumerator _FadeIn()
+        {
+            // 設定データのBGMボリュームに到達するまでループ
+            while (spawnedBgm.volume < GameDataManager.Inst.SettingData.BgmVolume)
+            {
+                spawnedBgm.volume += GameDataManager.Inst.SettingData.BgmVolume * (Time.deltaTime / FadeTime);
+
+                yield return null;
+            }
+
+            // 設定データの値を超えてしまっていた時のために代入処理
+            spawnedBgm.volume = GameDataManager.Inst.SettingData.BgmVolume;
+        }
+
+        /// <summary>
+        /// フェードアウト
+        /// </summary>
+        public void FadeOut()
+        {
+            StartCoroutine(_FadeOut());
+        }
+        IEnumerator _FadeOut()
+        {
+            // ０に到達するまでループ
+            while (spawnedBgm.volume > 0)
+            {
+                spawnedBgm.volume -= GameDataManager.Inst.SettingData.BgmVolume * (Time.deltaTime / FadeTime);
+
+                yield return null;
+            }
+
+            // ０を下回ってしまっていた時のために代入処理
+            spawnedBgm.volume = 0;
+        }
+
+        /// <summary>
+        /// BGM切り替え（フィーバータイム用）
+        /// </summary>
+        /// <param name="id">切り替えるBGMのID</param>
+        public void ChangeBgm(string id)
+        {
+            StartCoroutine(_ChangeBgm(id));
+        }
+        IEnumerator _ChangeBgm(string id)
+        {
+            // ０に到達するまでループ
+            while (spawnedBgm.volume > 0)
+            {
+                spawnedBgm.volume -= GameDataManager.Inst.SettingData.BgmVolume * (Time.deltaTime / FadeTime);
+
+                yield return null;
+            }
+
+            // ０を下回ってしまっていた時のために代入処理
+            spawnedBgm.volume = 0;
+
+            // 再生中のBGMを一時停止
+            PauseBgm();
+
+            // 指定されたBGMを再生
+            PlayBgm(id);
+
+            // フェードイン開始
+            FadeIn();
+        }
+
+        /// <summary>
+        /// 元のBGMに戻す（フィーバータイム用）
+        /// </summary>
+        public void ReturnBgm()
+        {
+            StartCoroutine(_ReturnBgm());
+        }
+        IEnumerator _ReturnBgm()
+        {
+            // ０に到達するまでループ
+            while (spawnedBgm.volume > 0)
+            {
+                spawnedBgm.volume -= GameDataManager.Inst.SettingData.BgmVolume * (Time.deltaTime / FadeTime);
+
+                yield return null;
+            }
+
+            // ０を下回ってしまっていた時のために代入処理
+            spawnedBgm.volume = 0;
+
+            // BGMを停止
+            StopBgm();
+
+            // 一時停止していたBGMを再生
+            ReStartBgm();
+
+            // フェードイン開始
+            FadeIn();
+        }
+
+        /// <summary>
+        /// 停止.
+        /// </summary>
+        public void StopBgm()
+        {
+            // 再生中でなければ処理を抜ける
+            if (!spawnedBgm.isPlaying) { return; }
+
+            spawnedBgm.Stop();
+
+            bgmPool.Despawn(spawnedBgm.transform);
+        }
+
+        /// <summary>
         /// ミュート時はボリューム0、非ミュート時は設定データの値を反映.
         /// </summary>
         void LateUpdate()
         {
             // まだBGMが再生されていないなら処理を抜ける
             if (!spawnedBgm) { return; }
-
-            if (GameDataManager.Inst.SettingData.IsBgmMute)
-            {
-                spawnedBgm.volume = 0;
-            }
-            else
-            {
-                spawnedBgm.volume = GameDataManager.Inst.SettingData.BgmVolume;
-            }
 
             // リザルト時
             if (isResult)
@@ -80,19 +227,6 @@ namespace VMUnityLib
                     isPlayedResult = true;
                 }
             }
-        }
-
-        /// <summary>
-        /// 停止.
-        /// </summary>
-        public void StopBgm()
-        {
-            // 再生中でなければ処理を抜ける
-            if (!spawnedBgm.isPlaying) { return; }
-
-            spawnedBgm.Stop();
-
-            bgmPool.Despawn(spawnedBgm.transform);
         }
     }
 }
