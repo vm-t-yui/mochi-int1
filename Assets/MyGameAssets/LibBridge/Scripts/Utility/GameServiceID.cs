2797b9d8a24b3b043932c7d10b64e5115d556792
TanakaMasaki <0sv65728326399x@ezweb.ne.jp>
2019-09-17 13:31:19 +0900

    IOSの実績・リーダーボードIDを追加、UI調整、ローカライズデータ更新

--- Assets/MyGameAssets/LibBridge/Scripts/Utility/GameServiceID.cs
+++ Assets/MyGameAssets/LibBridge/Scripts/Utility/GameServiceID.cs
@@ -1,22 +1,22 @@
 ﻿//== 実績・リーダーボード一覧 ================================================================================//
 // ＜実績１＞ 初めてのパンチ
 // ・ID (Android) : CgkIk5zJ6IMPEAIQAQ
-// ・ID (IOS)     : 
+// ・ID (IOS)     : firstPunch
 // ・解放条件     : 初プレイ時のリザルトで解放
 
 // ＜実績２＞ うさぎ界の救世主
 // ・ID (Android) : CgkIk5zJ6IMPEAIQAg
-// ・ID (IOS)     : 
+// ・ID (IOS)     : rabbitSavior
 // ・解放条件     : うさぎの合計救出回数が500を超えた時点でのリザルトで解放
 
 // ＜実績３＞ もちの破壊王
 // ・ID (Android) : CgkIk5zJ6IMPEAIQAw
-// ・ID (IOS)     : 
+// ・ID (IOS)     : destructionKing
 // ・解放条件     : 合計スコアが5,000を超えた時点でのリザルトで解放
 
 // ＜実績４＞ うさぎ博士
 // ・ID (Android) : CgkIk5zJ6IMPEAIQBA
-// ・ID (IOS)     : 
+// ・ID (IOS)     : rabbitDoctor
 // ・解放条件     : 図鑑コンプリート時のリザルトで解放
 
 
@@ -40,16 +40,16 @@ public class GameServiceID
     public const string ACHIEVEMENT_3 = "CgkIk5zJ6IMPEAIQAw";
     public const string ACHIEVEMENT_4 = "CgkIk5zJ6IMPEAIQBA";
 
-    public const string LEADERBOARD_1 = "";
-    public const string LEADERBOARD_2 = "";
+    public const string LEADERBOARD_1 = "CgkIk5zJ6IMPEAIQAA";
+    public const string LEADERBOARD_2 = "CgkIk5zJ6IMPEAIQBQ";
 #elif UNITY_IOS
-    public const string ACHIEVEMENT_1 = "unexpected_platform";
-    public const string ACHIEVEMENT_2 = "unexpected_platform";
-    public const string ACHIEVEMENT_3 = "unexpected_platform";
-    public const string ACHIEVEMENT_4 = "unexpected_platform";
+    public const string ACHIEVEMENT_1 = "firstPunch";
+    public const string ACHIEVEMENT_2 = "rabbitSavior";
+    public const string ACHIEVEMENT_3 = "destructionKing";
+    public const string ACHIEVEMENT_4 = "rabbitDoctor";
 
-    public const string LEADERBOARD_1 = "";
-    public const string LEADERBOARD_2 = "";
+    public const string LEADERBOARD_1 = "maximumBroken";
+    public const string LEADERBOARD_2 = "totalBroken";
 #else
     public const string ACHIEVEMENT_1 = "unexpected_platform";
     public const string ACHIEVEMENT_2 = "unexpected_platform";
